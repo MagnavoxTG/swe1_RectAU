295c8376bcd3f3cf3b82d397bc2b278750c8f299
MagnavoxTG <dominik.lohner92@gmx.at>
2019-10-02 20:52:44 +0200

    Release 1.0

---
+++ cmake-build-release/CMakeFiles/RectAU.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RectAU.dir/main.c.o"
+  "RectAU"
+  "RectAU.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/RectAU.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
